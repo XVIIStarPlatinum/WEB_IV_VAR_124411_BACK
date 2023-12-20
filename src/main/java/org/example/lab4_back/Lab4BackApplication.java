af8e78bdd94fa63aca10e57c734ff62c2fe4171e
Болорболд Аригуун Эркевич <bolorboldariguun16@gmail.com>
2023-12-20 03:19:19 +0300

    VER0.1: START

---
+++ src/main/java/org/example/lab4_back/Lab4BackApplication.java
@@ -0,0 +1,13 @@
+package org.example.lab4_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4BackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab4BackApplication.class, args);
+    }
+
+}
